e3cbbcc32291873302bad0b3ce5b5dc92095c74d
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-06-01 23:00:53 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, Button, Platform, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+      const [estado, setEstado] = useState('leitura');
+      const [anotacao, setAnotacao] = useState('Minha anotação');
+
+        return (
+          <View style={styles.container}>
+
+            {
+              estado == "leitura"?
+              <View>
+                <View>
+              <Text>Aplicativo de anotação</Text>
+              </View>
+
+              <View>
+                <Text>
+                  {anotacao}
+                </Text>
+              </View>
+
+              <TouchableOpacity>
+                <Text>
+                  +
+                </Text>
+              </TouchableOpacity>
+
+              </View>
+
+              :
+
+              estado == "atualizando"?
+              <View>
+
+              </View>
+
+              :
+
+              <View></View>
+
+            }
+           
+          </View>
+        ); 
+ 
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+
+  Image: {
+      width: '10px',
+      height: '10px',
+  }
+
 });
